80d5fbc485fd2a0314e745c9937129c288952bfe
XRohitHolnoor <rohithacker286@gmail.com>
2025-01-13 12:13:15 +0530

    Update youtube_manager_mongodb.py
    TODO completed

--- 12_python_mongodb/youtube_manager_mongodb.py
+++ 12_python_mongodb/youtube_manager_mongodb.py
@@ -22,7 +22,7 @@ def update_video(video_id, new_name, new_time):
     video_collection.update_one({'_id': ObjectId(video_id)}, {"$set": {"name": new_name, "time": new_time}})
 
 def delete_video(video_id):
-    video_collection.delete_one({"_id": video_id})
+    video_collection.delete_one({"_id": ObjectId(video_id)})
     # TODO: debug this video_id problem
 
 
